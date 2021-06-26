30a8b4bdb26c6b6a4142959613527f6b935d499a
Jonathan S Vargas <jonathan.vargas01@epn.edu.ec>
2021-06-25 20:20:22 -0500

    modificaciones de gandle

---
+++ 02-android/01-app/app/src/main/java/com/epn/moviles_2021a/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.epn.moviles_2021a
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
