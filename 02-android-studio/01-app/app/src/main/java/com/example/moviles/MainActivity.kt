40bb80964311e6130a9af2b188e86a95c432390e
Jonathan Vargas <jonathan.vargas01@epn.edu.ec>
2021-07-05 17:57:51 -0500

    tipos de layouts

--- 02-android-studio/01-app/app/src/main/java/com/example/moviles/MainActivity.kt
+++ 02-android-studio/01-app/app/src/main/java/com/example/moviles/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.moviles
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //encontrar un boton por ID
+        val bottonIrCicloVida = findViewById<Button>(
+            R.id.btn_ciclo_vida
+        )
+        //llamar a la fun cuando hace clic
+        bottonIrCicloVida.setOnClickListener {
+            abrirCicloVida();
+        }
+    }
+
+    fun abrirCicloVida(){
+        val intentExplicito = Intent(
+            this,
+            ACicloVida::class.java
+        )
+        startActivity(intentExplicito);
     }
 }
